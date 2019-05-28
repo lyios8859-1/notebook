64c6a638c817fb0c01401b2c314388d14a3e40c2
lyios88598-1 <194962218@qq.com>
2019-05-28 22:20:22 +0800

    字节转换

--- javascript/CustomEvent/custom_event.js
+++ javascript/CustomEvent/custom_event.js
@@ -1,53 +1,56 @@
-class LyEventDispatcher {
-  constructor() {
-    this.events = Object.create(null);
-  }
-  // 监听事件触发后，获取触发事件后传递过来的数据
-  addEventListener(type, handler) {
-    if (typeof handler !== "function") {
-      return;
-    }
-    this.events[type] = handler;
-  }
-  // 触发事件，传递数据
-  dispatchEvent(type, body) {
-    const e = Object.create(null);
-    e.body = body;
-    this.events[type](e);
-  }
+// 自定义事件监听
+
+function LyEventDispatcher() {
+  this.events = Object.create(null);
 }
+
+// 添加需要监听的事件
+LyEventDispatcher.prototype.LyAddEventListener = function(type, handler) {
+  if (typeof handler !== "function") {
+    return;
+  }
+  this.events[type] = handler;
+};
+
+// 触发定义的事件
+LyEventDispatcher.prototype.dispatchEvent = function(type, body) {
+  const e = Object.create(null);
+  e.body = body;
+  this.events[type](e);
+};
+
 let dispatch = new LyEventDispatcher();
-// 触发事件
-dispatch.dispatchEvent("lyTest", { name: "Jerry" });
-// 监听触发的事件
-dispatch.addEventListener("lyTest", function(e) {
+
+dispatch.LyAddEventListener("lyTest", function(e) {
   console.log("Test >>> ", e.body);
 });
 
-const eventUtil = {
-  // 触发事件 (事件类型，传输的数据)
-  notiyEvent(type, data) {
-    let event = document.createEvent("HTMLEvents");
-    event.initEvent("lyEvent");
-    event.eventType = type;
-    event.eventData = data;
-    // 触发事件
-    document.dispatchEvent(event);
-  },
-  // 监听事件的触发（执行） （事件类型，回调函数）
-  listenerEvent(type, callback) {
-    document.addEventListener(
-      "lyEvent",
-      event => {
-        if (event.eventType === type) {
-          console.log("data:", event.eventData);
-          callback && callback(event.eventData);
-        }
-      },
-      false
-    );
-  }
-};
+dispatch.dispatchEvent("lyTest", { name: "Jerry" });
+
+// const eventUtil = {
+//   // 触发事件 (事件类型，传输的数据)
+//   notiyEvent(type, data) {
+//     let event = document.createEvent("HTMLEvents");
+//     event.initEvent("lyEvent");
+//     event.eventType = type;
+//     event.eventData = data;
+//     // 触发事件
+//     document.dispatchEvent(event);
+//   },
+//   // 监听事件的触发（执行） （事件类型，回调函数）
+//   listenerEvent(type, callback) {
+//     document.addEventListener(
+//       "lyEvent",
+//       event => {
+//         if (event.eventType === type) {
+//           console.log("data:", event.eventData);
+//           callback && callback(event.eventData);
+//         }
+//       },
+//       false
+//     );
+//   }
+// };
 
 /**           
 // 自定义事件
