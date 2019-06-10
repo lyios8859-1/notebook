a2e4a56afbfdee4a4ca3e5fbe373147edff9ccff
lyios88598-1 <194962218@qq.com>
2019-06-10 22:03:57 +0800

    new CustomEvent 自定义事件

--- javascript/CustomEvent/custom_event.js
+++ javascript/CustomEvent/custom_event.js
@@ -1,3 +1,5 @@
+
+/*************************************************/
 // 自定义事件监听
 
 function LyEventDispatcher() {
@@ -5,7 +7,7 @@ function LyEventDispatcher() {
 }
 
 // 添加需要监听的事件
-LyEventDispatcher.prototype.LyAddEventListener = function(type, handler) {
+LyEventDispatcher.prototype.LyAddEventListener = function (type, handler) {
   if (typeof handler !== "function") {
     return;
   }
@@ -13,7 +15,7 @@ LyEventDispatcher.prototype.LyAddEventListener = function(type, handler) {
 };
 
 // 触发定义的事件
-LyEventDispatcher.prototype.dispatchEvent = function(type, body) {
+LyEventDispatcher.prototype.dispatchEvent = function (type, body) {
   const e = Object.create(null);
   e.body = body;
   this.events[type](e);
@@ -21,11 +23,16 @@ LyEventDispatcher.prototype.dispatchEvent = function(type, body) {
 
 let dispatch = new LyEventDispatcher();
 
-dispatch.LyAddEventListener("lyTest", function(e) {
+dispatch.LyAddEventListener("lyTest", function (e) {
   console.log("Test >>> ", e.body);
 });
 
 dispatch.dispatchEvent("lyTest", { name: "Jerry" });
+
+/*************************************************/
+
+
+
 
 // const eventUtil = {
 //   // 触发事件 (事件类型，传输的数据)
@@ -52,7 +59,7 @@ dispatch.dispatchEvent("lyTest", { name: "Jerry" });
 //   }
 // };
 
-/**           
+/**
 // 自定义事件
 (function() {
   if (typeof window.CustomEvent === "undefined") {
@@ -91,5 +98,23 @@ window.dispatch = function(event, params) {
 window.addEventListener("ly", event => {
   console.log(".....", event.detail);
 });
+
+
+function lyEvents(params, eventName = 'lyEvent') {
+  // 这是一个浏览器事件
+  let lyEvent = new CustomEvent(eventName, {
+    bubbles: true, // 是否冒泡
+    cancelable: true, // 是否可以取消该事件
+    detail: params // 需传递的数据
+  });
+  // 触发
+  document.dispatchEvent(lyEvent);
+}
+lyEvents({ name: 'ejfl' })
+// 监听事件
+document.addEventListener('lyEvent', function (ev) {
+  console.log('数据：', ev.detail)
+});
+
 
  */
--- skillOperat/README.md
+++ skillOperat/README.md
@@ -204,3 +204,20 @@ function hasBottom() {
   return pageHeight - viewportHeight - scrollHeight < 20;
 }
 ```
+
+## 字节转换
+
+````javascript
+// K KB MB GB ...转换
+function bytesToSize(bytes) {
+  if (bytes == 0) {
+    return 0 + "B";
+  }
+  let k = 1000; // or 1024
+  let sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  let i = Math.floor(Math.log(bytes) / Math.log(k));
+  return (bytes / Math.pow(k, i)).toPrecision(3) + "" + sizes[i];
+}
+
+console.log(bytesToSize(2000));
+```
