90dd7e0f4024a847c2477deba257e1aaf3f67fb2
lyios8859-1 <194962218@qq.com>
2020-01-09 00:30:43 +0800

    :sparkles:

--- node操作excel/readFile.js
+++ node操作excel/readFile.js
@@ -103,6 +103,38 @@ objReadline.on('close', ()=>{
 */
 
 
+// 递归拷贝目录下所有文件和目录
+var fs=require('fs');
+var copy=function(src,dst){
+  let paths = fs.readdirSync(src); //同步读取当前目录
+  paths.forEach(function(path){
+    var _src=src+'/'+path;
+    var _dst=dst+'/'+path;
+    fs.stat(_src,function(err,stats){ //stats 该对象 包含文件属性
+      if(err)throw err;
+      if(stats.isFile()){ //如果是个文件则拷贝
+        let readable=fs.createReadStream(_src);//创建读取流
+        let writable=fs.createWriteStream(_dst);//创建写入流
+        readable.pipe(writable);
+      }else if(stats.isDirectory()){ //是目录则 递归
+        checkDirectory(_src,_dst,copy);
+      }
+    });
+  });
+}
+var checkDirectory=function(src,dst,callback){
+  fs.access(dst, fs.constants.F_OK, (err) => {
+    if(err){
+      fs.mkdirSync(dst);
+      callback(src,dst);
+    }else{
+      callback(src,dst);
+    }
+   });
+};
+const  SOURCES_DIRECTORY = 'd:commonPrefab'; //源目录
+checkDirectory(SOURCES_DIRECTORY,__dirname,copy);
+
 
 
 var fs = require('fs');
