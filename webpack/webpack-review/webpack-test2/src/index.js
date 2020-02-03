7573dd96baf356846de7c6ff49ae9474aaaaf764
lyios8859-1 <194962218@qq.com>
2020-02-03 22:19:04 +0800

    :data:

---
+++ webpack/webpack-review/webpack-test2/src/index.js
@@ -0,0 +1,9 @@
+import src from './1.jpeg';
+import './font/iconfont.css'
+import './index.less'; // 这样是全局部引入是没有作用的
+const img = new Image();
+img.src = src;
+img.classList.add('imgStyle');
+
+
+document.querySelector('#root').append(img);
