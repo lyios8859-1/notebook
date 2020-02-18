71008f04b9f27271e1abdaebc6ad3fe0db0d263c
lyios8859-1 <194962218@qq.com>
2020-02-18 21:57:39 +0800

    :sparkles:

--- jestlean/test-demo/jest-vue/src/containers/TodoList/__tests__/unit/TodoList.test.js
+++ jestlean/test-demo/jest-vue/src/containers/TodoList/__tests__/unit/TodoList.test.js
@@ -1,7 +1,6 @@
 // shallowMount 不会渲染子组件（适合Tdd/单元测试），mount 会渲染子组件（适合BDD/集成测试）
-import { shallowMount } from '@vue/test-utils';
-import TodoList from '../../TodoList.vue';
-
+import { shallowMount } from '@vue/test-utils'
+import TodoList from '../../TodoList.vue'
 
 describe('TodoList.vue', () => {
   it('组件渲染 props.msg 通过', () => {
@@ -9,7 +8,6 @@ describe('TodoList.vue', () => {
     const wrapper = shallowMount(TodoList, {
       propsData: { msg }
     })
-    
-    console.log(wrapper.text());
+    console.log(wrapper.text())
   })
 })
